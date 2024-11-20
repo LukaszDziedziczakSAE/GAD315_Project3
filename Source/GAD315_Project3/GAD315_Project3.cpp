838037ab2d7b5d69511fd5ec066bd40afa546bbd
Lukasz Dziedziczak <wukash@gmail.com>
2024-11-20 19:49:20 +1030

    Project Created

---
+++ Source/GAD315_Project3/GAD315_Project3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAD315_Project3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAD315_Project3, "GAD315_Project3" );
